648be117f28e4ff8d6574bcdba055c223d7a3656
ankit <ankitmistry@qable.io>
2020-03-26 18:46:56 +0530

    Add Data to DB

--- src/test/java/testng/SendDataOnDB.java
+++ src/test/java/testng/SendDataOnDB.java
@@ -1,39 +1,160 @@
 package testng;
 
-import org.json.JSONObject;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import okhttp3.*;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+import java.io.IOException;
+import java.nio.file.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+
 
 public class SendDataOnDB {
 
     TestngWatcher testngWatcher=new TestngWatcher();
-    public void createBuild(){
+    //public static String URL="http://v2.tesbo.io:7000";
+    public static String URL="http://localhost:7000/";
+    public static String buildID;
+
+    @Parameter(names={"--userKey", "-u"})
+    public static String userKey;
+    @Parameter(names={"--projectKey", "-p"})
+    public static String projectKey;
+    @Parameter(names={"--reportDirectory", "-d"})
+    public static String reportDirectory;
+    @Parameter(names={"--buildName", "-b"})
+    public static String buildName;
+
+
+    public void setArgument(String[] args) throws Exception {
+        SendDataOnDB setCommandLineArgument=new SendDataOnDB();
+        JCommander.newBuilder()
+                .addObject(setCommandLineArgument)
+                .build()
+                .parse(args);
+        argumentsValidation();
+    }
+
+    public void argumentsValidation() throws Exception {
+        if(userKey == null | projectKey == null | projectKey == null){
+            throw new Exception("Please add all required [userKey, projectKey, projectKey] arguments!!!");
+        }
+    }
+
+
+
+
+    public void createBuild() throws Exception {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        System.out.println("+++++++++++++++++++++++++++>>>>>>  "+ "{\n\t\"buildName\": \""+buildName+"\",\n\t\"projectID\" : \""+projectKey+"\",\n\t\"userID\" : \""+userKey+"\"\n}");
+        RequestBody body = RequestBody.create(mediaType, "{\n\t\"buildName\": \""+buildName+"\",\n\t\"projectID\" : \""+projectKey+"\",\n\t\"userID\" : \""+userKey+"\"\n}");
+        Request request = new Request.Builder()
+                .url(URL+"/createBuild")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONParser parser = new JSONParser();
+            JSONObject object = null;
+            try {
+                object = (JSONObject) parser.parse(response.body().string());
+                if(object.get("errors")!= null){
+                    throw new Exception(object.get("message").toString());
+                }
+                buildID = (object.get("buildID")).toString();
+                response.close();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void createTest(String file)
-    {
+    public void addDataInDataBase(String file) throws Exception {
         if(testngWatcher.checkFileChanged(file)){
             String teatData;
             ConvertXmlToJson convertXmlToJson=new ConvertXmlToJson();
             teatData = convertXmlToJson.convertXmlFileDataToJson(convertXmlToJson.readXmlFile(file));
+            JSONParser parser = new JSONParser();
+            JSONObject testngData = (JSONObject) parser.parse(teatData);
 
+            createBuild();
+            createTests(new JSONObject(testngData));
+            updateBuild();
             System.out.println("=============================================================================================");
-            System.out.println(teatData);
+            System.out.println(testngData);
             System.out.println("=============================================================================================");
         }
     }
 
-    public static void main(String[] args) throws NoSuchFieldException {
-        String file="E:/demo/DemoTNG/DemoTNG/DemoTNG/test-output";
-        if(!Files.exists(Paths.get(file))){
-            throw new NoSuchFieldException("Directory is not found on following path: "+file);
+    public void createTests(JSONObject testObject) {
+
+
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, "{\n\t\"buildKey\": \""+buildID+"\",\n\t\"userKey\" : \""+userKey+"\",\n\t\"projectKey\":\""+projectKey+"\",\n\t\"testsList\":\""+testObject+"\",\n\t\"projectType\":\"TestNG\",\n\t\"buildName\":\""+buildName+"\"\n\t\n}");
+            Request request = new Request.Builder()
+                    .url(URL+"/createTests")
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+            Response response = client.newCall(request).execute();
+            response.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void updateBuild() {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n\t\"buildKey\": \""+buildID+"\"\n}");
+        Request request = new Request.Builder()
+                .url(URL+"/updateBuild")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            response.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] arguments) throws Exception {
         SendDataOnDB sendDataOnDB=new SendDataOnDB();
+
+        System.out.println("======> arguments: "+ arguments.length);
+        for(String arg:arguments){
+            System.out.println("===> "+arg);
+        }
+        sendDataOnDB.setArgument(arguments);
+
+
+        if(!Files.exists(Paths.get(reportDirectory))){
+            throw new NoSuchFieldException("Directory is not found on following path: "+reportDirectory);
+        }
+
         while (true) {
-            sendDataOnDB.createTest(file);
+            sendDataOnDB.addDataInDataBase(reportDirectory);
         }
     }
 
